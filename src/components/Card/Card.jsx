25dc1ce1bdffb38482deb0dd9f31bfe5ffd4aa8e
Beth Presten <bethpresten@gmail.com>
2021-01-25 10:12:25 -0500

    added containers for home, about, contact, about and routes on app.js for those containers, installed react-router-dom

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Card = ({ title, link, image, description, alt }) => {
+  return (
+    <>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={image} className="card-img-top" alt={alt} />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="{link}" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar() {
+  return <div>My navbar will go here</div>;
+}
+
+export default Navbar;
