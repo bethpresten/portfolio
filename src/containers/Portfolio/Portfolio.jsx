05dc12b0b002d08c6d60e7c85b1aa089dece3d3d
Beth Presten <bethpresten@gmail.com>
2021-01-27 17:55:04 -0500

    added two gifs and the links to the projects.json

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -14,10 +14,35 @@ function Portfolio() {
       <div className="row">
         <div className="col-sm-6">
           <ProjectCard
+            id={projects[6].id}
+            key={projects[6].id}
+            name={projects[6].name}
+            image={projects[6].image}
+            alt={projects[6].alt}
+            description={projects[6].description}
+            link={projects[6].link}
+          />
+        </div>
+        <div className="col-sm-6">
+          <ProjectCard
+            id={projects[7].id}
+            key={projects[7].id}
+            name={projects[7].name}
+            image={projects[7].image}
+            alt={projects[7].alt}
+            description={projects[7].description}
+            link={projects[7].link}
+          />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6">
+          <ProjectCard
             id={projects[0].id}
             key={projects[0].id}
             name={projects[0].name}
             image={projects[0].image}
+            alt={projects[0].alt}
             description={projects[0].description}
             link={projects[0].link}
           />
@@ -28,6 +53,7 @@ function Portfolio() {
             key={projects[1].id}
             name={projects[1].name}
             image={projects[1].image}
+            alt={projects[1].alt}
             description={projects[1].description}
             link={projects[1].link}
           />
@@ -40,6 +66,7 @@ function Portfolio() {
             key={projects[2].id}
             name={projects[2].name}
             image={projects[2].image}
+            alt={projects[2].alt}
             description={projects[2].description}
             link={projects[2].link}
           />
@@ -50,6 +77,7 @@ function Portfolio() {
             key={projects[3].id}
             name={projects[3].name}
             image={projects[3].image}
+            alt={projects[3].alt}
             description={projects[3].description}
             link={projects[3].link}
           />
@@ -62,6 +90,7 @@ function Portfolio() {
             key={projects[4].id}
             name={projects[4].name}
             image={projects[4].image}
+            alt={projects[4].alt}
             description={projects[4].description}
             link={projects[4].link}
           />
@@ -72,6 +101,7 @@ function Portfolio() {
             key={projects[5].id}
             name={projects[5].name}
             image={projects[5].image}
+            alt={projects[5].alt}
             description={projects[5].description}
             link={projects[5].link}
           />
@@ -82,79 +112,3 @@ function Portfolio() {
 }
 
 export default Portfolio;
-
-// <div className="row">
-// <div className="col-sm-4">
-//   <img
-//     src="./assets/portfolio-images/All the yummy burgers!.gif"
-//     className="image-fluid"
-//     alt="burger app functionality"
-//     id="eat-da-burgers"
-//   />
-// </div>
-// <div className="col-sm-5">
-//   <h3>Eat Da Burger</h3>
-//   <h5>
-//     JavaScript + JQuery application that utilizes the open weather API.
-//     Application stores user info upon button click to deliver the
-//     current and 5-day forecast for the inputted city. Previous searched
-//     city is saved as a button so the user can search the same city with
-//     ease.
-//   </h5>
-// </div>
-// </div>
-// <div className="row">
-// <div className="col-sm-4">
-//   <img
-//     src="./assets/portfolio-images/CodingQuiz.png"
-//     className="image-fluid"
-//     alt="coding quiz"
-//     id="coding-quiz"
-//   />
-// </div>
-// <div className="col-sm-5">
-//   <h3>Coding Quiz</h3>
-//   <h5>
-//     JavaScript created application that utilizes local storage and timer
-//     functionalities. User is scored based on time elapsed and correct
-//     versus incorrect questions. Scores are saved and generated on a high
-//     scores html.
-//   </h5>
-// </div>
-// </div>
-// <div className="row">
-// <div className="col-sm-4">
-//   <img
-//     src="./assets/portfolio-images/Password Generator.gif"
-//     className="image-fluid"
-//     alt="password generator"
-//     id="password-generator"
-//   />
-// </div>
-// <div className="col-sm-5">
-//   <h3>Random Password Generator</h3>
-//   <h5>
-//     JavaScript generated application that utilizes user input to deliver
-//     a random password.
-//   </h5>
-// </div>
-// <div className="row">
-//   <div className="col-sm-4">
-//     <img
-//       src="./assets/portfolio-images/weatherapi.png"
-//       className="image-fluid"
-//       alt="weather dashboard"
-//       id="weather-dashboard"
-//     />
-//   </div>
-//   <div className="col-sm-5">
-//     <h3>Weather Dashboard</h3>
-//     <h5>
-//       JavaScript + JQuery application that utilizes the open weather
-//       API. Application stores user info upon button click to deliver the
-//       current and 5-day forecast for the inputted city. Previous
-//       searched city is saved as a button so the user can search the same
-//       city with ease.
-//     </h5>
-//   </div>
-// </div> */}
