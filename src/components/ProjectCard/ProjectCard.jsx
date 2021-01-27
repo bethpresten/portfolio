6626c568f82128af697f06773f7f019b33581a0f
Beth Presten <bethpresten@gmail.com>
2021-01-27 18:03:00 -0500

    changed the word title to name to get the project name to appear

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = ({ title, link, image, description, alt }) => {
+const Card = ({ name, link, image, description, alt }) => {
   return (
     <>
       <div className="card" style={{ width: "18rem" }}>
         <img src={image} className="card-img-top" alt={alt} />
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
+          <h5 className="card-title">{name}</h5>
           <p className="card-text">{description}</p>
           <a href="{link}" className="btn btn-primary">
             Github Repo
